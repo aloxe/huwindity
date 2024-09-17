e7b25b8c184b0acb0f5cf3e1f3265bbbb8d37ffb
aloxe <github@blog.re>
2024-09-17 23:50:14 +0200

    organise things in folders

--- .eleventy.js
+++ .eleventy.js
@@ -8,21 +8,29 @@ module.exports = function(eleventyConfig) {
   }
 
   // Passthrough
-  eleventyConfig.addPassthroughCopy({ "src/static": "." });
-
+  eleventyConfig.addPassthroughCopy({ "src/assets": "." });
+  eleventyConfig.addPassthroughCopy({ 'src/_assets/public': '/' });
+  eleventyConfig.addPassthroughCopy({ 'src/_assets/img': '/img' });
+  eleventyConfig.addPassthroughCopy({ 'src/_assets/fonts': '/fonts' });
+  eleventyConfig.addPassthroughCopy({ 'src/_assets/css': '/css' })
+  
   // Watch targets
   eleventyConfig.addWatchTarget("./src/styles/");
 
-  var pathPrefix = "";
-  if (process.env.GITHUB_REPOSITORY) {
-    pathPrefix = process.env.GITHUB_REPOSITORY.split('/')[1];
-  }
+  // var pathPrefix = "";
+  // if (process.env.GITHUB_REPOSITORY) {
+  //   pathPrefix = process.env.GITHUB_REPOSITORY.split('/')[1];
+  // }
 
   return {
     dir: {
-      input: "src"
+      input: "src/pages",
+      output: '_site',
+      layouts: '../_layouts',
+      includes: '../_layouts',
     },
-    pathPrefix
+    templateFormats: ['md', 'njk', 'jpg', 'gif', 'png', 'html'],
+    markdownTemplateEngine: 'njk',
   }
 };
 
