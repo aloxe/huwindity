e9a266707a802feb965844bfd5bebf2355fa4b19
aloxe <github@blog.re>
2024-09-18 18:12:34 +0200

    make tailwind work from 11ty config

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,9 @@
 const fs = require("fs");
 const htmlmin = require("html-minifier-terser");
+const tailwind = require('tailwindcss');
+const postCss = require('postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 
 module.exports = function(eleventyConfig) {
 
@@ -12,20 +16,22 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ 'src/_assets/public': '/' });
   eleventyConfig.addPassthroughCopy({ 'src/_assets/img': '/img' });
   eleventyConfig.addPassthroughCopy({ 'src/_assets/fonts': '/fonts' });
-  eleventyConfig.addPassthroughCopy({ 'src/_assets/css': '/css' })
   
   // Watch targets
-  eleventyConfig.addWatchTarget("./src/styles/");
+  eleventyConfig.addWatchTarget("./src/_assets/css/");
+
+  // process css
+  eleventyConfig.addNunjucksAsyncFilter('postcss', postcssFilter);
 
   return {
     dir: {
       input: "src/pages",
-      output: '_site',
       layouts: '../_layouts',
-      includes: '../_layouts',
+      includes: '../_includes',
+      data: '../_data',
+      output: '_site',
     },
     templateFormats: ['md', 'njk', 'jpg', 'gif', 'png', 'html'],
-    markdownTemplateEngine: 'njk',
   }
 };
 
@@ -39,4 +45,23 @@ function htmlminTransform(content, outputPath) {
     return minified;
   }
   return content;
+}
+
+// inspired by https://zenzes.me/eleventy-integrate-postcss-and-tailwind-css/
+const postcssFilter = (cssCode, done) => {
+  postCss([
+    require('postcss-import'),
+    tailwind(require('./tailwind.config')),
+    autoprefixer(),
+    // TODO use purgecss for each layout
+    // cssnano({ preset: 'default' })
+  ])
+    .process(cssCode, {
+      // path to our CSS file
+      from: './src/_assets/css/styles.css'
+    })
+    .then(
+      (r) => done(null, r.css),
+      (e) => done(e, null)
+    );
 }
