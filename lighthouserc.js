85f3c853fc1bbf8e156948a733c4d863c9155919
aloxe <github@blog.re>
2024-10-07 19:04:25 +0200

    use staticDir with url
    try with recomended

--- lighthouserc.js
+++ lighthouserc.js
@@ -6,6 +6,7 @@ module.exports = {
         "http://localhost/index.html",
         "http://localhost/404.html"
       ],
+      staticDistDir: './_site',
       staticDirFileDiscoveryDepth: 1,
       "numberOfRuns": 1,
     },
@@ -15,16 +16,16 @@ module.exports = {
     assert: {
     // assert options here
     // preset: 'lighthouse:no-pwa',
-    // "preset": "lighthouse:recommended",
+    "preset": "lighthouse:recommended",
     // https://stackoverflow.com/questions/65550806/how-can-i-set-lighthouse-ci-to-only-test-accessibility
-      "assertions": {
-        'first-contentful-paint': ["warn", {"maxNumericValue": 4000}],
-        'categories:performance': ['warn', { minScore: 1 }], // not valid ?
-        'categories:accessibility': ['warn', { minScore: 1 }],
-        'categories:best-practices': ['warn', { minScore: 1 }],
-        'categories:seo': ['warn', { minScore: 1 }],
-        "categories:pwa": ["warn", { minScore: 1 }],
-      }
+      // "assertions": {
+      //   'first-contentful-paint': ["warn", {"maxNumericValue": 4000}],
+      //   'categories:performance': ['warn', { minScore: 1 }], // not valid ?
+      //   'categories:accessibility': ['warn', { minScore: 1 }],
+      //   'categories:best-practices': ['warn', { minScore: 1 }],
+      //   'categories:seo': ['warn', { minScore: 1 }],
+      //   "categories:pwa": ["warn", { minScore: 1 }],
+      // }
     },
     // server: {
     //   // server options here
