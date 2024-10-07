4ca9504f1f1d693561d62a27f521be6d43fc3cc5
aloxe <github@blog.re>
2024-10-07 15:53:17 +0200

    upudate lighthouse settings a bit.

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,12 +2,20 @@ module.exports = {
   ci: {
     collect: {
       // collect options here
+      // "url": [
+      //   "/"
+      // ],
       staticDirFileDiscoveryDepth: 1,
-      },
+      "numberOfRuns": 1,
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
     assert: {
-      // assert options here
-      // preset: 'lighthouse:no-pwa',
-      // https://stackoverflow.com/questions/65550806/how-can-i-set-lighthouse-ci-to-only-test-accessibility
+    // assert options here
+    // preset: 'lighthouse:no-pwa',
+    // "preset": "lighthouse:recommended",
+    // https://stackoverflow.com/questions/65550806/how-can-i-set-lighthouse-ci-to-only-test-accessibility
       "assertions": {
         'first-contentful-paint': ["warn", {"maxNumericValue": 4000}],
         'categories:performance': ['warn', { minScore: 1 }], // not valid ?
@@ -16,9 +24,6 @@ module.exports = {
         'categories:seo': ['warn', { minScore: 1 }],
         "categories:pwa": ["warn", { minScore: 1 }],
       }
-    },
-    upload: {
-      target: 'temporary-public-storage',
     },
     // server: {
     //   // server options here
