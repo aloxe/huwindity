3d987ff6ebf0b7221cdab9a070ca83a82fe8f281
aloxe <github@blog.re>
2024-10-07 21:57:24 +0200

    correct some of the lighthouse problems
    lighthouse config cleanup

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,10 +1,9 @@
 module.exports = {
   ci: {
     collect: {
-      // collect options here
       "url": [
         "http://localhost/index.html",
-        "http://localhost/404.html"
+        "http://localhost/documentation/",
       ],
       staticDistDir: './_site',
       staticDirFileDiscoveryDepth: 1,
@@ -14,10 +13,7 @@ module.exports = {
       target: 'temporary-public-storage',
     },
     assert: {
-    // assert options here
     preset: 'lighthouse:no-pwa',
-    // "preset": "lighthouse:recommended",
-    // https://stackoverflow.com/questions/65550806/how-can-i-set-lighthouse-ci-to-only-test-accessibility
       "assertions": {
         'first-contentful-paint': ["warn", {"maxNumericValue": 4000}],
         'categories:performance': ['warn', { minScore: 1 }], // not valid ?
@@ -25,13 +21,10 @@ module.exports = {
         'categories:best-practices': ['warn', { minScore: 1 }],
         'categories:seo': ['warn', { minScore: 1 }],
         "categories:pwa": ["warn", { minScore: 1 }],
+        "render-blocking-resources": "off",
+        "color-contrast": ['warn', { minScore: 0.9 }],
+        "errors-in-console": "warn",
       }
     },
-    // server: {
-    //   // server options here
-    // },
-    // wizard: {
-    //   // wizard options here
-    // },
   },
 };
