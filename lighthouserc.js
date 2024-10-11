f76c92e7a3e781a9316d4e8969cfed11b8d1348e
aloxe <github@blog.re>
2024-10-11 11:06:43 +0200

    make it 100 100 100 100
    rename images to fit with manifest
    
    attempt to Defer non-critical CSS to Eliminate render-blocking resources
    
    new lighthouse test
    
    revert css changes
    
    try out preload css
    
    revert css changes

--- lighthouserc.js
+++ lighthouserc.js
@@ -13,18 +13,7 @@ module.exports = {
       target: 'temporary-public-storage',
     },
     assert: {
-    preset: 'lighthouse:no-pwa',
-      "assertions": {
-        'first-contentful-paint': ["warn", {"maxNumericValue": 4000}],
-        'categories:performance': ['warn', { minScore: 1 }], // not valid ?
-        'categories:accessibility': ['warn', { minScore: 1 }],
-        'categories:best-practices': ['warn', { minScore: 1 }],
-        'categories:seo': ['warn', { minScore: 1 }],
-        "categories:pwa": ["warn", { minScore: 1 }],
-        "render-blocking-resources": "off",
-        "color-contrast": ['warn', { minScore: 0.9 }],
-        "errors-in-console": "warn",
-      }
+    preset: 'lighthouse:recommended',
     },
   },
 };
